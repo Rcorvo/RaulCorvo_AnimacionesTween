3b45543c563ecf4ae283aff3eba23f7c4474c531
Rcorvo <rcu1987@gmail.com>
2016-11-19 16:08:36 +0100

    Actividad 4.9 terminada

---
+++ app/src/main/java/com/example/animacionestween/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.animacionestween;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView texto = (TextView) findViewById(R.id.textView);
+        Animation animacion = AnimationUtils.loadAnimation(this,
+                R.anim.animacion);
+        texto.startAnimation(animacion);
+    }
+}
